4246835b63a9f8958c6c4bd28d868f1989da6d76
PeterWong <peter@peterwongpp.com>
2011-11-05 18:06:28 +0800

    create an initial dummy test application without applying the flushing-flash's code

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '8f1a12a50983d4424f7ce2b6b4b5e07cdad4bf5ac6db07e37fd5c1feef1ac5ff69b9ed69db90c44f8ecf93457e1e899912ae8f68f417956895f2b0ea01a3fb56'
