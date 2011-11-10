87170757a9a3a129e1bfc2eef28deace219e0730
PeterWong <peter@peterwongpp.com>
2011-11-10 16:36:13 +0800

    add dummy application for testing 'eating' devise's flash messages

---
+++ test/dummy-with-devise/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyWithDevise::Application.config.secret_token = 'db1e5d94dd3181b773bc3073513a614162ec4f8c589d83c794fca9c376fac89a2e6b241fbe5faf6ada7225e3231e9b1c360af2836f02ad0bc4fe667fdd30219e'
